dc1d1bc8bce1aa26fc78b5f54092bc8806b50835
Amalaelangovan <104757578+Amalaelangovan@users.noreply.github.com>
2022-06-16 23:51:26 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Azuretest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int a = 94;
+            int b = 108;
+            Console.WriteLine(a + b);
+            Console.ReadLine();
+        }
+    }
+}
